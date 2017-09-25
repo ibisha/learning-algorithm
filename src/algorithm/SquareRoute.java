187f835ecfb36cb371f1389d26337913f941d384
ibisha <akashiroakaaka@gmail.com>
2017-09-26 00:35:18 +0900

    最小値探索に変更

--- src/algorithm/SquareRoute.java
+++ src/algorithm/SquareRoute.java
@@ -1,8 +1,11 @@
 package algorithm;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -28,6 +31,43 @@ import java.util.stream.Stream;
 public class SquareRoute {
 
   public static void main(String[] args) throws Exception {
+    List<int[]> rows = readFromSysin();
+    // List<int[]> rows = readFromFile("格子.txt");
+    // List<int[]> rows = createRandomRoute(1000);
+
+    long start = System.currentTimeMillis();
+    final int size = rows.size();
+    
+    // ひし形に変換
+    List<int[]> diamond = convertToDiamond(rows);
+    for (int[] r: diamond) {
+      for (int n : r) {
+        System.out.print(n + " ");
+      }
+      System.out.println();
+    }
+
+    // 探索と結果の出力
+    search(diamond, size);
+    for (int[] r: diamond) {
+      for (int n : r) {
+        System.out.print(n + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println(diamond.get(diamond.size() - 1)[0]);
+
+    long end = System.currentTimeMillis();
+    // System.out.println("elapsed = " + (end - start) + "ms");
+  }
+
+  /**
+   * 標準入力から経路を作成する
+   * 
+   * @return 経路
+   */
+  private static List<int[]> readFromSysin() {
     List<int[]> rows = new ArrayList<>();
     try (Scanner in = new Scanner(System.in);) {
       while (true) {
@@ -38,23 +78,60 @@ public class SquareRoute {
         rows.add(cols);
       }
     }
-    // Random r = new Random();
-    // for (int k = 0; k < side; k++) {
-    // int[] row = new int[side];
-    // for (int i = 0; i < side; i++) {
-    // row[i] = r.nextInt(10);
-    // }
-    // rows.add(row);
-    // }
+    return rows;
+  }
 
-    long start = System.currentTimeMillis();
+  /**
+   * 経路を書いたファイルを読み込む
+   * 
+   * @param name
+   *          ファイル名
+   * @return 経路
+   * @throws IOException
+   */
+  private static List<int[]> readFromFile(String name) throws IOException {
+    List<int[]> rows = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(name))) {
+      String line = br.readLine();
+      while (line != null) {
+        int[] cols = Stream.of(line.split("")).mapToInt(Integer::parseInt).toArray();
+        rows.add(cols);
+        line = br.readLine();
+      }
+    }
+    return rows;
+  }
 
-    // 1000*1000にすると1分弱かかる
-    final int size = rows.size();
-    System.out.println(size);
+  /**
+   * ランダムに経路を作成する
+   * 
+   * @param size
+   *          経路の1辺の大きさ
+   * @return 経路
+   */
+  private static List<int[]> createRandomRoute(int size) {
+    List<int[]> rows = new ArrayList<>();
+    Random r = new Random();
+    for (int k = 0; k < size; k++) {
+      int[] row = new int[size];
+      for (int i = 0; i < size; i++) {
+        row[i] = r.nextInt(10);
+      }
+      rows.add(row);
+    }
+    return rows;
 
-    // ピラミッド型に変換
+  }
+
+  /**
+   * 正方形をひし形に変換する
+   * 
+   * @param rows
+   * @return ひし形
+   */
+  private static List<int[]> convertToDiamond(List<int[]> rows) {
     List<int[]> diamond = new ArrayList<>();
+    int size = rows.size();
     int[] row = null;
     for (int i = 0; i < size; i++) {
       row = new int[i + 1];
@@ -71,34 +148,7 @@ public class SquareRoute {
       diamond.add(row);
     }
 
-    long now = System.currentTimeMillis();
-    System.out.println("経路作成まで" + (now - start) + "ms");
-
-    // 行列確認
-    System.out.println("----before----");
-    for (int[] r : diamond) {
-      for (int n : r) {
-        System.out.print(n + " ");
-      }
-      System.out.println();
-    }
-
-    // 探索と結果の出力
-    search(diamond, size);
-    // 探索後確認
-    System.out.println("----after----");
-    for (int[] r : diamond) {
-      for (int n : r) {
-        System.out.print(n + " ");
-      }
-      System.out.println();
-    }
-
-    System.out.println("----結果----");
-    System.out.println(diamond.get(diamond.size() - 1)[0]);
-
-    long end = System.currentTimeMillis();
-    System.out.println("elapsed = " + (end - start) + "ms");
+    return diamond;
   }
 
   /**
@@ -106,8 +156,10 @@ public class SquareRoute {
    * 頂点から対角上の頂点までの経路の最大値を探索する。
    * 結果は、引数のListの末尾の値にセットされる。
    * 
-   * @param route 探索したい経路
-   * @param size 正方形の辺の長さ
+   * @param route
+   *          探索したい経路
+   * @param size
+   *          正方形の辺の長さ
    */
   private static void search(List<int[]> route, int size) {
     topToBottom(route, size);
@@ -131,17 +183,13 @@ public class SquareRoute {
    * @param pyramid
    */
   private static void topToBottom(List<int[]> pyramid, int end) {
-    int tempMemory;
     for (int i = 0; i < end - 1; i++) {
-      tempMemory = 0;
       int[] current = pyramid.get(i);
       int[] next = pyramid.get(i + 1);
       next[0] = current[0] + next[0];
-      for (int j = 0; j < current.length; j++) {
-        next[j] = Math.max(next[j], current[j] + tempMemory);
-
-        tempMemory = next[j + 1];
-        next[j + 1] = current[j] + next[j + 1];
+      next[next.length - 1] = current[current.length -1] + next[next.length - 1];
+      for (int j = 1; j < current.length; j++) {
+        next[j] = Math.min(next[j] + current[j - 1], next[j] + current[j]);
       }
     }
   }
@@ -160,7 +208,7 @@ public class SquareRoute {
       int[] next = pyramid.get(i + 1);
 
       for (int j = 0; j < current.length - 1; j++) {
-        next[j] = Math.max(next[j] + current[j], next[j] + current[j + 1]);
+        next[j] = Math.min(next[j] + current[j], next[j] + current[j + 1]);
       }
     }
   }
