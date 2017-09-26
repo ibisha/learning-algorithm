e29d77014e2d9bca6d1e144efcf622fdf773ba09
ibisha <akashiroakaaka@gmail.com>
2017-09-26 10:02:46 +0900

    with open に変更

--- src/algorithm/pyramid_route.py
+++ src/algorithm/pyramid_route.py
@@ -4,11 +4,15 @@ import time
  
 # read file
 rows = []
-FILE = open("test/p67-triangle.txt", "r")
-for blob in FILE: rows.append([int(i) for i in blob.split(" ")])
+FILE = "test/p67-triangle.txt"
+with open(FILE, "r") as f:
+    for blob in f:
+        # スペースで分割
+        rows.append([int(i) for i in blob.split(" ")])
  
 start = time.time()
  
+# 底辺から頂点に向かって計算する
 for i,j in [(i,j) for i in range(len(rows)-2,-1,-1) for j in range(i+1)]:
     rows[i][j] +=  max([rows[i+1][j],rows[i+1][j+1]])
  
